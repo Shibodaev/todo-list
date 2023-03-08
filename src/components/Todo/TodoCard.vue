310698b5bf592f352975c0de4d18aada1f2b9213
shibodaev <mike756@mail.ru>
2023-03-08 21:47:54 +0300

    часть функций

--- src/components/Todo/TodoCard.vue
+++ src/components/Todo/TodoCard.vue
@@ -2,83 +2,80 @@
   <div
     class="flex flex-col card-task border border-solid border-gray-200 rounded-lg box-border w-full max-w-xs mt-4 overflow-hidden"
   >
-    <div class="card-task__head bg-green-300 p-4 flex items-center">
-      <div class="flex flex-col w-full">
-        <div class="w-full block">
-          <input
-            class="p-2 box-border text-xs border-b bg-transparent border-gray-100"
-            v-show="noView"
-            type="text"
-            v-model="taskTitle"
-          />
-          <span
-            @click="noView = !noView"
-            class="float-right h-4 w-4 transititext-primary text-primary transition duration-75 mt-2 hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
-            data-te-toggle="tooltip"
-            title="Hi! I'm tooltip"
-          >
-            <font-awesome-icon
-              class="w-full h-full align-top pointer-events-none"
-              :icon="noView ? ['fa', 'fa-pen-to-square'] : ['fa', 'fa-pen']"
-            />
-          </span>
-          <span
-            class="overflow-hidden break-words"
-            :class="[noView ? 'w-full block' : 'w-auto float-left']"
-            >{{ taskTitle }}</span
-          >
-        </div>
-      </div>
+    <div class="card-task__head flex items-center">
+      <form class="w-full block p-4 border-b bg-transparent border-black-800">
+        <input
+          class="p-2 box-border text-xs w-full"
+          type="text"
+          placeholder="Добавить задачу"
+          :value="inputValue"
+          v-on:input="handlerInput($event)"
+        />
+      </form>
     </div>
     <div class="card-task__body p-4 flex flex-col">
       <div class="card-task__container">
-        <input
-          class="w-full p-2 box-border text-xs border-b bg-transparent border-gray-600"
-          type="text"
-        />
+        <template v-if="tasks">
+          <task-input-vue
+            v-for="(item, index) in tasks"
+            :key="index"
+            :id="index"
+            :item="item.task"
+            @click="deleteElement($event, item.id)"
+          ></task-input-vue>
+        </template>
       </div>
-      <!-- <button
-        class="w-full flex items-center border border-gray-500 justify-center p-3 rounded-lg border-solid hover:bg-green-200 hover:border-green-200 font-semibold ease-in-out duration-200 active:bg-green-500"
+      <button
+        @click="addTask()"
+        class="w-full flex items-center border border-gray-500 justify-center p-3 rounded-lg border-solid hover:bg-green-200 hover:border-green-200 font-semibold ease-in-out duration-200 active:bg-green-500 mt-4"
       >
         Add Task
-      </button> -->
+      </button>
     </div>
   </div>
 </template>
 
 <script>
+import TaskInputVue from './TaskInput.vue';
+
 export default {
+  components: {
+    TaskInputVue,
+  },
   data() {
     return {
       tasks: [],
-      taskTitle: '',
-      noView: true,
-      valueInput: '',
-      input:
-        " <input class='p-2 box-border text-xs border-b bg-transparent border-gray-100' v-show='noView' type='text' v-model='task' @change='getTask(event)'/>",
+      inputValue: null,
     };
   },
   computed: {},
   methods: {
-    getTask(event) {
-      console.log(event);
+    addTask() {
+      if (!this.inputValue) return;
+      this.tasks.push({
+        id: this.tasks.length,
+        task: this.inputValue,
+      });
+      this.inputValue = '';
     },
-  },
-  props: {
-    title: {
-      type: String,
-      default: '',
+    handlerInput(event) {
+      this.inputValue = event.target.value;
     },
-    inputValue: {
-      type: String,
-      default: '',
+    deleteElement(e) {
+      if (!e.target.attributes['data-delete']) return;
+      this.tasks.splice(e.currentTarget.getAttribute('data-parent'), 1);
+      return this.tasks;
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.card-task__head input {
-  width: calc(100% - 40px);
+.card-task__head {
+  &:hover {
+    span {
+      @apply opacity-100 pointer-events-auto;
+    }
+  }
 }
 </style>
